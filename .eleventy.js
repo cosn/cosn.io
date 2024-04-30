7904f98a9a993e6a9d9e1c30231ff83d9233bff2
Cosmin Nicolaescu <cos@cosn.dev>
2024-04-30 16:52:50 -0700

    update footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,38 @@
-const pluginCss = require("eleventy-postcss-extension");
+const pluginCss = require('eleventy-postcss-extension')
+const Image = require('@11ty/eleventy-img')
+const moment = require('moment')
 
 module.exports = function (config) {
-  config.addPlugin(pluginCss);
-  config.addFilter("date", function (date, format) {
-    const moment = require("moment");
-    return moment(date).format(format);
-  });
+  config.addPlugin(pluginCss)
+
+  config.addFilter('date', function (date, format) {
+    return moment(date).format(format)
+  })
+
+  config.addShortcode('currentYear', function () {
+    return moment().utc().format('YYYY')
+  })
+
+  config.addNunjucksAsyncShortcode('social', async (url, img) => {
+    const metadata = await Image(`src/assets/logos/${img}.svg`, {
+      formats: ['svg'],
+      svgShortCircuit: true,
+      dryRun: true,
+    })
+    const svg = metadata.svg[0].buffer.toString()
+
+    return `<a
+      class="transition hover:fill-teal-500 dark:hover:text-teal-400 fill-zinc-500 dark:fill-zinc-400"
+      href="${url}" target="_blank">${svg}
+    </a>`
+  })
 
   return {
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
 
     dir: {
-      input: "src",
+      input: 'src',
     },
-  };
-};
+  }
+}
