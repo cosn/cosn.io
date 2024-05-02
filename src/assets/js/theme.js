a9af9dd28712baa52961bdc6e6c25c1078c6bbf8
Cosmin Nicolaescu <cos@cosn.dev>
2024-05-02 10:52:57 -0700

    Update theme switcher

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -1,47 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const themeToggleButton = document.getElementById('theme-toggle');
-  const iconLight = document.getElementById('theme-icon-moon');
-  const iconDark = document.getElementById('theme-icon-sun');
+  const themeToggleButton = document.getElementById('theme-toggle')
+  const iconLight = document.getElementById('theme-icon-moon')
+  const iconDark = document.getElementById('theme-icon-sun')
+  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light'
 
   function applyTheme(theme) {
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark');
-      iconLight.classList.remove('hidden');
-      iconDark.classList.add('hidden');
-      themeToggleButton.setAttribute('aria-label', 'Switch to light theme');
-    } else if (theme === 'light') {
-      document.documentElement.classList.remove('dark');
-      iconLight.classList.add('hidden');
-      iconDark.classList.remove('hidden');
-      themeToggleButton.setAttribute('aria-label', 'Switch to dark theme');
-    } else { // system theme
-      const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      document.documentElement.classList.toggle('dark', systemPrefersDark);
-      if (systemPrefersDark) {
-        iconLight.classList.remove('hidden');
-        iconDark.classList.add('hidden');
-        themeToggleButton.setAttribute('aria-label', 'Switch to light theme');
-      } else {
-        iconLight.classList.add('hidden');
-        iconDark.classList.remove('hidden');
-        themeToggleButton.setAttribute('aria-label', 'Switch to dark theme');
-      }
+    if (theme === systemTheme) {
+      localStorage.setItem('theme', 'system')
+    } else {
+      localStorage.setItem('theme', theme)
     }
-    localStorage.setItem('theme', theme);
+
+    if (theme === 'system') {
+      theme = systemTheme
+    }
+
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+      document.documentElement.style.colorScheme = 'dark'
+      iconLight.classList.remove('hidden')
+      iconDark.classList.add('hidden')
+      themeToggleButton.setAttribute('aria-label', 'Switch to light theme')
+    } else {
+      document.documentElement.classList.remove('dark')
+      document.documentElement.style.colorScheme = 'light'
+      iconLight.classList.add('hidden')
+      iconDark.classList.remove('hidden')
+      themeToggleButton.setAttribute('aria-label', 'Switch to dark theme')
+    }
   }
 
   function initTheme() {
-    const storedTheme = localStorage.getItem('theme') || 'system';
-    applyTheme(storedTheme);
+    const storedTheme = localStorage.getItem('theme') || 'system'
+    applyTheme(storedTheme)
   }
 
   function toggleTheme() {
-    const currentTheme = localStorage.getItem('theme');
-    const newTheme = currentTheme === 'dark' ? 'light' : (currentTheme === 'light' ? 'system' : 'dark');
-    applyTheme(newTheme);
+    let currentTheme = localStorage.getItem('theme')
+    if (currentTheme === 'system') {
+      currentTheme = systemTheme
+    }
+
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
+    applyTheme(newTheme)
   }
 
-  themeToggleButton.addEventListener('click', toggleTheme);
+  themeToggleButton.addEventListener('click', toggleTheme)
 
-  initTheme();
-});
+  initTheme()
+})
