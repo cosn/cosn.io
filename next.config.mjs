77da3895faec85715bf1256c582baf30f6ecc083
Cosmin Nicolaescu <cos@cosn.dev>
2024-05-10 15:57:20 -0700

    Revert "Set up github pages"
    This reverts commit 8c6bd8dbe7f0b91bd2cb86b2e982cea1d272beb3.

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  output: "export",
-  basePath: "/cosn.github.io",
 }
 
 const withMDX = nextMDX({
