d1cb602c38a5ec972a06460430d021c727f6cfa1
Cosmin Nicolaescu <cos@cosn.dev>
2024-05-10 15:16:10 -0700

    Update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,6 +7,7 @@ import { Container } from '@/components/Container'
 import {
   GitHubIcon,
   LinkedInIcon,
+  MailIcon,
   XIcon,
 } from '@/components/SocialIcons'
 import portraitImage from '@/images/portrait.jpg'
@@ -35,21 +36,10 @@ function SocialLink({
   )
 }
 
-function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        fillRule="evenodd"
-        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
-      />
-    </svg>
-  )
-}
-
 export const metadata: Metadata = {
   title: 'About',
   description:
-    'I’m Spencer Sharp. I live in New York City, where I design the future.',
+    "Hello!",
 }
 
 export default function About() {
@@ -68,36 +58,73 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-2xl dark:text-zinc-100">
-            I’m Spencer Sharp. I live in New York City, where I design the
-            future.
+            Cosmin Nicolaescu
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              I'm an engineer, operator, and entrepreneur.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              Most recently I was the CTO of <span className="font-bold">Brex</span>, where I scaled the company from 40 to over 1,000 employees and hundreds of millions in annual revenue. I've also worked at <span className="font-bold">Stripe</span> as an early engineer and manager, building core global payments infrastructure, and launching the Stripe Terminal product. I started my career at <span className="font-bold">Microsoft</span>, working on the initial versions of Azure and Office365.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
+              Moving places has helped me embrace changes of varying magnitudes. I've so far lived in the following cities:
             </p>
-            <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
-            </p>
+            <ul className="list-disc ml-8">
+              <li>Bucharest, Romania</li>
+              <li>Deatsville, AL</li>
+              <li>Philadelphia, PA</li>
+              <li>Vancouver, BC</li>
+              <li>Seattle, WA</li>
+              <li>San Francisco, CA</li>
+              <li>Los Angeles, CA</li>
+            </ul>
+          </div>
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <p>Some opinions and beliefs I've formed over time:</p>
+            <ul className="list-disc ml-8">
+              <li><span className="italic">Everything compounds</span>
+                <ul className="list-disc ml-4">
+                  <li>Investing in something, and then letting it grow, leads to surprising moats</li>
+                  <li>Conversely, a series of poor decisions or constant <a href="https://en.wikipedia.org/wiki/Tacking_(sailing)" target="_blank" className="underline">tacking</a> is tremendously difficult to recover from</li>
+                </ul>
+              </li>
+              <li className="mt-2"><span className="italic">Time is our most precious resource</span>
+                <ul className="list-disc ml-4">
+                  <li>At least for now, you can't buy more, nor manipulate</li>
+                  <li>Memories are more valuable than vast majority of material goods</li>
+                </ul>
+              </li>
+              <li className="mt-2"><span className="italic">Speed matters</span>
+                <ul className="list-disc ml-4">
+                  <li>In a multi-step problem, reduce the long-pole, recursively</li>
+                  <li>Everything can move faster, though you should focus on accelerating the things that move the needle</li>
+                  <li>How fast you act and respond is a signal to others</li>
+                </ul>
+              </li>
+              <li className="mt-2"><span className="italic">Smaller teams are better</span>
+                <ul className="list-disc ml-4">
+                  <li>There's less overhead, leads to faster decisions, and is more fun for everyone involved</li>
+                  <li>Scarcity of resources increases better prioritization and focus</li>
+                  <li>The failure modes from under-staffing tend to be much less severe than those from over-hiring</li>
+                </ul>
+              </li>
+              <li className="mt-2"><span className="italic">Extreme positions often lead to suboptimal outcomes</span>
+                <ul className="list-disc ml-4">
+                  <li>Most often you're simply balancing multiple variables, which leads to various tradeoffs</li>
+                  <li>People tend to over-simplify problems (also people, i.e. "person X is good/bad")</li>
+                  <li>In a conflict, the truth is somewhere in the middle</li>
+                  <li>Few situations are zero-sum, and you should strive to avoid them</li>
+                </ul>
+              </li>
+              <li className="mt-2"><span className="italic">EQ tends to be underrated</span>
+                <ul className="list-disc ml-4">
+                  <li>Individuals have unique reactions, so no matter how great one is at pattern-matching, outcomes are highly dependent on the mix of individuals involved</li>
+                  <li>Being able to accurately read the room is a significant advantage, especially in tech</li>
+                  <li>Learn to trust your instincts, and constantly fine-tune them with new experiences</li>
+                </ul>
+              </li>
+            </ul>
           </div>
         </div>
         <div className="lg:pl-20">
@@ -116,11 +143,11 @@ export default function About() {
               icon={MailIcon}
               className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
             >
-              Email
+              Send email
             </SocialLink>
           </ul>
         </div>
       </div>
-    </Container>
+    </Container >
   )
 }
