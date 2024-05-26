c7f3895cbf240da664b2684b12ef61a790d97c8b
Cosmin Nicolaescu <cos@cosn.dev>
2024-05-26 14:06:57 -0700

    update desc

--- src/lib/meta.ts
+++ src/lib/meta.ts
@@ -2,5 +2,5 @@ export const meta = {
   author: 'Cosmin Nicolaescu',
   email: 'blog@cosn.io',
   title: 'Pragmatic Optimism',
-  description: 'Observations from building software and companies, plus the occasional random life experiences.',
+  description: 'Observations from building software and companies, plus the occasional life experiences.',
 }
