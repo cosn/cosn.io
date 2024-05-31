ccdc3142306db049b96026b4fe6e97a9c71bbfec
Cosmin Nicolaescu <cos@cosn.dev>
2024-05-31 12:10:51 -0700

    add mobile browsers for e2e

--- e2e/integration.spec.ts
+++ e2e/integration.spec.ts
@@ -8,6 +8,12 @@ test('check homepage', async ({ page }) => {
 
 test('loads posts', async ({ page }) => {
   await page.goto('/')
+
+  const isMenuVisible = await page.isVisible('text=Menu')
+  if (isMenuVisible) {
+    await page.click('text=Menu')
+  }
+
   await page.getByRole('link', { name: 'Posts' }).click()
   await expect(page.getByRole('article')).not.toHaveCount(0)
 })
