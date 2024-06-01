2638b6a757765a2d24104b18d52a64eb599de30b
Cosmin Nicolaescu <cos@cosn.dev>
2024-06-01 15:47:12 -0700

    update logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,28 +1,22 @@
-import { createLogger, format, transports } from 'winston';
+import { createLogger, format, transports } from 'winston'
 
 const logger = createLogger({
   level: 'info',
   format: format.combine(
     format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
+      format: 'YYYY-MM-DD HH:mm:ss',
     }),
     format.errors({ stack: true }),
     format.splat(),
-    format.json()
+    format.json(),
   ),
   transports: [
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()),
+    }),
     new transports.File({ filename: 'error.log', level: 'error' }),
-    new transports.File({ filename: 'combined.log' })
-  ]
-});
+    new transports.File({ filename: 'combined.log' }),
+  ],
+})
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new transports.Console({
-    format: format.combine(
-      format.colorize(),
-      format.simple()
-    )
-  }));
-}
-
-export default logger;
+export default logger
