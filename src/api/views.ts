8f79c53807ff6a5b3daffbbdb69e79dbb5444bda
Cosmin Nicolaescu <cos@cosn.dev>
2024-06-01 15:33:13 -0700

    Add winston logging library

--- src/api/views.ts
+++ src/api/views.ts
@@ -1,12 +1,15 @@
 'use server'
 
 import redis from '@/lib/redis'
+import logger from '@/lib/logger'
 
 export async function incrementViews(slug: string): Promise<number> {
+  logger.info(`Incrementing views for ${slug}`)
   return redis.hincrby('views', slug, 1)
 }
 
 export async function getViews(slug: string): Promise<number> {
+  logger.info(`Getting views for ${slug}`)
   const views = await redis.hget('views', slug)
   return Number(views)
 }
