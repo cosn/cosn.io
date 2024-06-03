729cfd5b914a466556086b79a8974fd6138bcce6
Cosmin Nicolaescu <cos@cosn.dev>
2024-06-03 08:15:05 -0700

    still not rendering server-side

--- src/components/PostLayout.tsx
+++ src/components/PostLayout.tsx
@@ -60,7 +60,7 @@ export function PostLayout({
   })
 
   return (
-    <Container className="mt-16 lg:mt-32">
+    <Container excludeClerk={true} className="mt-16 lg:mt-32">
       <div className="xl:relative">
         <div className="mx-auto max-w-2xl">
           {previousPathname && (
