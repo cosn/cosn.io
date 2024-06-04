0162f414ffc9e42d09e691e7ace662a380e40289
Cosmin Nicolaescu <cos@cosn.dev>
2024-06-03 21:07:11 -0700

    update tests

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -22,49 +22,46 @@ describe('siteUrl', () => {
 
   it('should return the base site URL if no path is provided', () => {
     const url = siteUrl()
-
     expect(url).toBe('http://localhost')
   })
 
   it('should return the full URL with the provided path', () => {
     const url = siteUrl('about')
-
     expect(url).toBe('http://localhost/about')
   })
 
   it('should remove the leading slash from the path', () => {
     const url = siteUrl('/about')
-
     expect(url).toBe('http://localhost/about')
   })
 
   it('should handle an empty string path correctly', () => {
     const url = siteUrl('')
-
     expect(url).toBe('http://localhost/')
   })
 
   it('should handle multiple leading slashes in the path', () => {
     const url = siteUrl('///about')
-
     expect(url).toBe('http://localhost/about')
   })
 
   it('should handle paths with query parameters', () => {
     const url = siteUrl('/about?foo=bar')
-
     expect(url).toBe('http://localhost/about?foo=bar')
   })
 
   it('should handle paths with hash fragments', () => {
     const url = siteUrl('/about#section1')
-
     expect(url).toBe('http://localhost/about#section1')
   })
 
   it('should handle paths with both query parameters and hash fragments', () => {
     const url = siteUrl('/about?foo=bar#section1')
-
     expect(url).toBe('http://localhost/about?foo=bar#section1')
+  })
+
+  it('should return the full URL with multiple paths', () => {
+    const url = siteUrl('about', '/hello', 'world')
+    expect(url).toBe('http://localhost/about/hello/world')
   })
 })
