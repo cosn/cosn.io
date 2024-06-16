72acb70d6f272713f13179a5025cb7bed7d2424c
Cosmin Nicolaescu <cos@cosn.dev>
2024-06-16 20:02:34 +0100

    move prettier to package

--- prettier.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Options} */
-module.exports = {
-  arrowParens: 'always',
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'all',
-  plugins: ['prettier-plugin-tailwindcss'],
-  tailwindFunctions: ['clsx', 'cn'],
-}
