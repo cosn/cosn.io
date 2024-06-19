aff0084280890c68736f6e485b3566a8d7d3ecfa
Cosmin Nicolaescu <cos@cosn.dev>
2024-06-19 16:43:26 +0200

    fix: update number of posts on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ function Photos() {
 export default async function Home() {
   const res = await fetch(siteUrl('api/posts'), { next: { tags: ['posts'] } })
   const data = await res.json()
-  const posts: PostWithSlug[] = data.posts.slice(0, 4)
+  const posts: PostWithSlug[] = data.posts.slice(0, 3)
 
   return (
     <>
