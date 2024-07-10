5682c8cdf80591bfb3cae217a687960f81f34f43
Cosmin Nicolaescu <cos@cosn.dev>
2024-07-10 16:02:18 -0700

    ci: remove redish_rest_token check

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -4,10 +4,6 @@ if (!process.env.UPSTASH_REDIS_REST_URL) {
   throw new Error('Missing UPSTASH_REDIS_REST_URL')
 }
 
-if (!process.env.UPSTASH_REDIS_REST_TOKEN) {
-  throw new Error('Missing UPSTASH_REDIS_REST_TOKEN')
-}
-
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
